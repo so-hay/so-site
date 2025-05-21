94772e7618098fb829a05148afa987a9519b5d37
so-hay <hay.soraka@gmail.com>
2025-05-21 13:46:03 +0900

    k

---
+++ so-site1/src/app/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-sky-100">
+      <h1 className="text-4xl font-bold text-sky-800">Hello Next.js + Tailwind!</h1>
+    </div>
+  );
+}
